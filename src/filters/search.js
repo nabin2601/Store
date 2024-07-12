dbe5a713e53dc477dc2abb7bc3150745cbaf22b4
nabin2601 <nabinb943@gmail.com>
2024-07-12 11:16:30 +1000

    filter search

--- src/filters/search.js
+++ src/filters/search.js
@@ -1,7 +1,29 @@
 import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
 const setupSearch = (product) => {
-    
+    const input = getElement('.search-input');
+    const form = getElement('.input-form');
+
+    form.addEventListener('keyup', () => {
+      const value = input.value;
+
+      if (value) {
+        const newStore = product.filter((store) => {
+          const { name } = store;
+          if (name.startsWith(value)) {
+            return store;
+          }
+        });
+        display(newStore, getElement('.products-container'));
+        if (newStore.length < 1) {
+          getElement('.products-container').innerHTML = `
+              <h3 class="filter-error">Sorry, there is no item to display in your search </h3>
+            `;
+        }
+      } else {
+        display(product, getElement('.products-container'));
+      }
+    });
 };
 
 export default setupSearch;
