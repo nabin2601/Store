304e4857505369235c4de0d3325a96562e702a60
nabin2601 <nabinb943@gmail.com>
2024-07-12 12:49:57 +1000

    filter products

--- src/filters/companies.js
+++ src/filters/companies.js
@@ -1,6 +1,33 @@
 import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
 
-const setupCompanies = () => {};
+const setupCompanies = (store) => {
+  let companies = ['all', ...new Set(store.map((product) => product.company))];
+  const companiesDOM = getElement('.companies');
+
+  companiesDOM.innerHTML = companies
+    .map((company) => {
+      return `
+        <button class="company-btn">${company}</button>
+        `;
+    })
+    .join('');
+  companiesDOM.addEventListener('click', (e) => {
+    const el = e.target;
+    if (el.classList.contains('company-btn')) {
+      let newStore = [];
+      if (el.textContent === 'all') {
+        newStore = [...store];
+        // console.log(newStore);
+      } else {
+        newStore = store.filter(
+          (product) => product.company === el.textContent
+        );
+        // console.log(newStore);
+      }
+      display(newStore, getElement('.products-container'));
+    }
+  });
+};
 
 export default setupCompanies;
